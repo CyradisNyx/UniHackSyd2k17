1695b5d95c6d36367c0768edaa286254b6faea13
CyradisNyx <sarahbeavan@gmail.com>
2017-08-19 19:57:32 +1000

    More models stuff

--- apiStuff/models.py
+++ apiStuff/models.py
@@ -1,5 +1,5 @@
 """Define Database Model Structures."""
-from apiStuff import db, app
+from apiStuff import db, app, helpers
 
 
 class Article(db.Model):
@@ -10,6 +10,7 @@ class Article(db.Model):
 
     Args:
         source_url (str): source to scrape from
+        event (Event): Event object article is part of
 
     Attributes:
         article_id (int): Integer representation/primary key
@@ -18,19 +19,26 @@ class Article(db.Model):
         event (db.Relationship): Instance of Event
         source (str): Company/Site Source
         source_url (str): Link to article
+        date (DateTime): Date of article posting
+        content (text): Text field containing article
     """
 
     __tablename__ = "article"
     article_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     rating = db.Column(db.Integer)
-    event = db.Column(db.Integer, db.ForeignKey('event.event_id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('event.event_id'))
+    event = db.relationship("Event")
     source = db.Column(db.String(20))
     source_url = db.Column(db.String(80))
+    content = db.Column(db.Text)
 
-    def __init__(self, source_url):
+    def __init__(self, source_url, event):
         """Assign Variables."""
         self.source_url = source_url
+        self.event = event
+
+        scrapeData = helpers.scrape(source_url)
 
 
 class Event(db.Model):
@@ -43,5 +51,18 @@ class Event(db.Model):
         idek
 
     Attributes:
-        idek
+        event_id (int): Integer representation/primary_key
+        title (str): title of event
+        date (str): date of creation (date of event)
+        articles (db.Relationship): related articles
     """
+
+    __tablename__ = "event"
+    event_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    date = db.Column(db.DateTime)
+    articles = db.relationship("Article", backref="event", lazy="dynamic")
+
+    def __init__(self):
+        """Assign Variables."""
+        pass
