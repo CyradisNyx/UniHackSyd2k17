4abeb0c3dbf61446892b99448bfefad95950cb48
MikhailFedulov <mikhail_fedulov@hotmail.com>
2017-08-20 04:39:39 +1000

    front end problem

--- unihack/views.py
+++ unihack/views.py
@@ -9,3 +9,12 @@ from unihack import app
 def home():
     """Route for homepage."""
     return render_template('index.html')
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+  app.run(debug=True)
