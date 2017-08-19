aac90d53cb1fe9f4e0f9083bed056df8ac74e76a
josh <longjosh1990@gmail.com>
2017-08-19 20:29:34 +1000

    get_rating updated to query

--- apiStuff/helpers.py
+++ apiStuff/helpers.py
@@ -1,11 +1,14 @@
 """Helper Variables and Functions."""
 
 import numpy as np
+
+
+from flask_sqlalchemy import SQLAlchemy
 from apiStuff import db, models
 
 import indicoio
-def get_rating(article):
+def get_rating(id):
 	indicoio.config.api_key = '11214320f4b6ce1e81f46ed4a570c7e0'
-	x = (indicoio.political())
+	x = indicoio.political(models.article.query.get(id).content)
 	rating = (x['Liberal']/(x['Liberal'] + x['Conservative'])) * 10
 	return (rating)
