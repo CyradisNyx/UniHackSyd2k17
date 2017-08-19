464927a867a8a2febcf2e60dd8d9eeca1ab4d58d
josh <longjosh1990@gmail.com>
2017-08-19 20:11:37 +1000

    get_rating function draft

--- apiStuff/helpers.py
+++ apiStuff/helpers.py
@@ -1,1 +1,11 @@
 """Helper Variables and Functions."""
+
+import numpy as np
+from apiStuff import db, models
+
+import indicoio
+def get_rating(article):
+	indicoio.config.api_key = '11214320f4b6ce1e81f46ed4a570c7e0'
+	x = (indicoio.political())
+	rating = (x['Liberal']/(x['Liberal'] + x['Conservative'])) * 10
+	return (rating)
