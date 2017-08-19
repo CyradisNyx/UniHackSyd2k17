4b2a5dfceecc91731add80bcf7e196575ecc6a6e
MikhailFedulov <mikhail_fedulov@hotmail.com>
2017-08-20 06:02:17 +1000

    sge

--- unihack/views.py
+++ unihack/views.py
@@ -15,3 +15,15 @@ def home():
 def page_not_found(e):
     """404 Page Not Found."""
     return render_template('404.html'), 404
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
